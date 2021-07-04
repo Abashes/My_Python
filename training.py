c6d88bd450763cfa3cc6838e00ea1a6ea19ee82b
Abashes <48415837+Abashes@users.noreply.github.com>
2021-07-04 10:17:40 +0530

    Update training.py
    Added import files

--- training.py
+++ training.py
@@ -1,1 +1,14 @@
+import numpy as np
+import pandas as pd
+import scipy as sp
+from scipy import stats
+from scipy.stats import norm, skew
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
